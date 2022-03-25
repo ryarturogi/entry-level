21bdcbce1b4157dd403799fb8f28586959aab7b6
Ricardo Guillen <ricardo.guillen@elaniin.com>
2022-03-25 18:59:32 -0400

    Update tailwindcss.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.png')",
+      },
+      maxWidth: {
+        hero: '76.25rem',
+      },
+      colors: {
+        accent: {
+          100: '#6791ff',
+          500: '#6872ff',
+          800: '#2b3298',
+        },
+        notice: {
+          success: '#00d6c0',
+          warn: '#ffc549',
+          info: '#6791ff',
+          'danger-100': '#ff5c49',
+          'danger-200': '#cb3903',
+          'danger-300': '#b60000',
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/typography')],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
 }
