c7bc2cd3b7223e3320fc520e1551c7924c0918fc
Ricardo Guillen <ricardo.guillen@elaniin.com>
2022-03-25 19:06:00 -0400

    Import font-stack from adobe-fonts

---
+++ pages/_document.jsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <link rel="stylesheet" href="https://use.typekit.net/ooy8veu.css" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
