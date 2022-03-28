048d30151b8ed605198ec37464d46fe1236bab68
Ricardo Guillen <ricardo.guillen@elaniin.com>
2022-03-28 15:33:25 -0400

    Fix JobsCard linter errors

--- components/Jobs/JobCard.jsx
+++ components/Jobs/JobCard.jsx
@@ -1,37 +1,44 @@
-import { BookmarkIcon } from "@heroicons/react/outline";
-import { ArrowRightIcon } from "@heroicons/react/solid";
-import Link from "next/link";
+import { BookmarkIcon } from '@heroicons/react/outline';
+import { ArrowRightIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
 
-import JobTags from "@/components/Jobs/JobTags";
-import Avatar from "@/components/UI/Avatar";
-import Button from "@/components/UI/Button";
-import { formatDate, isToday, timeSince } from "@/utils/formatDate";
+import JobTags from '@/components/Jobs/JobTags';
+import Avatar from '@/components/UI/Avatar';
+import Button from '@/components/UI/Button';
+import { formatDate, isToday, timeSince } from '@/utils/formatDate';
+
+const jobStatus = (job) => {
+  switch (true) {
+    case job.isGuaranteed:
+      return 'border-b-[2rem] sm:border-b-0 sm:border-l-[4rem] border-accent-100';
+    case job.isFeatured:
+      return 'border-l-[4.5rem] border-accent-800';
+    default:
+      return '';
+  }
+};
 
 function JobCard({ job }) {
   return (
     <li
-      className={` max-w-4xl mx-auto relative flex flex-col justify-center w-full px-5 py-3.5 bg-white rounded-2xl sm:rounded-3xl shadow-md shadow-slate-300 ${
-        job.isGuaranteed
-          ? "border-b-[2rem] sm:border-b-0 sm:border-l-[4rem] border-accent-100"
-          : job.isFeatured
-          ? "border-l-[4.5rem] border-accent-800"
-          : ""
-      }`}
+      className={`
+        max-w-4xl mx-auto relative flex flex-col justify-center w-full px-5 py-3.5 bg-white rounded-2xl sm:rounded-3xl shadow-md shadow-slate-300
+      ${jobStatus(job)}`}
       style={{
-        backgroundColor: job.hasCompanyColor.isActive ? job.hasCompanyColor.color : "#fff",
-        border: job.hasCompanyColor.isActive ? "none" : "",
-        color: job.hasCompanyColor.isActive ? "#fff" : "#000"
+        backgroundColor: job.hasCompanyColor.isActive ? job.hasCompanyColor.color : '#fff',
+        border: job.hasCompanyColor.isActive ? 'none' : '',
+        color: job.hasCompanyColor.isActive ? '#fff' : '#000',
       }}
     >
       <div
         className={`lg:flex items-center space-x-5 ${
-          (job.isFeatured || job.isGuaranteed) && !job.hasCompanyColor.isActive ? "-ml-14" : "ml-2"
+          (job.isFeatured || job.isGuaranteed) && !job.hasCompanyColor.isActive ? '-ml-14' : 'ml-2'
         }`}
       >
         <div className="flex flex-col items-center justify-start w-full space-x-5 space-y-3 lg:justify-between sm:space-y-0 sm:items-start lg:items-start sm:flex-row">
           <div className="self-center">
             {job.hasCompanyLogo && (
-              <Link href={`jobs/${job.id}`}>
+              <Link href={`job/${job.id}`}>
                 <a>
                   <Avatar avatar={job.companyLogo} size="md" />
                 </a>
@@ -49,7 +56,7 @@ function JobCard({ job }) {
                   <small className="text-xs font-medium capitalize">Guaranteed</small>
                 )}
 
-                <Link href={`/jobs/${job.id}`}>
+                <Link href={`/job/${job.id}`}>
                   <a className="">
                     <div className="text-sm font-light leading-snug hover:text-accent-500">
                       {job.companyName}
@@ -64,7 +71,7 @@ function JobCard({ job }) {
                 {job.jobTags && (
                   <JobTags
                     tags={job.jobTags}
-                    theme={job.hasCompanyColor.isActive ? "light" : "dark"}
+                    theme={job.hasCompanyColor.isActive ? 'light' : 'dark'}
                   />
                 )}
               </section>
@@ -80,7 +87,7 @@ function JobCard({ job }) {
 
             <hr
               className={`w-full h-1 lg:w-1 lg:h-[6rem] rounded-xl ${
-                job.hasCompanyColor.isActive ? "bg-white" : "bg-gray-100"
+                job.hasCompanyColor.isActive ? 'bg-white' : 'bg-gray-100'
               }`}
             />
 
@@ -89,7 +96,7 @@ function JobCard({ job }) {
                 <BookmarkIcon />
               </Button>
 
-              <Link href={`jobs/${job.id}`}>
+              <Link href={`job/${job.id}`}>
                 <a className="p-2 text-white rounded-full w-9 h-9 bg-notice-danger-100 hover:bg-green-500">
                   <ArrowRightIcon />
                 </a>
