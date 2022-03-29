85ad0b5241607e4c1e52e8743a88088caa4f51b8
Ricardo Guillen <ricardo.guillen@elaniin.com>
2022-03-29 18:20:12 -0400

    Fix linter errors formatDate utils

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,70 +1,82 @@
+/* eslint-disable no-param-reassign */
 export const formatDate = (dateString) => {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' }
-  return new Date(dateString).toLocaleDateString(undefined, options)
-}
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+
+  // eslint-disable-next-line no-undefined
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
 
 export const timeSince = (time) => {
   switch (typeof time) {
     case 'number':
-      break
+      break;
     case 'string':
-      time = +new Date(time)
-      break
+      time = Number(new Date(time));
+      break;
     case 'object':
-      if (time.constructor === Date) time = time.getTime()
-      break
+      if (time.constructor === Date) {
+        time = time.getTime();
+      }
+      break;
     default:
-      time = +new Date()
+      time = Number(new Date());
   }
-  let time_formats = [
-    [60, 'seconds', 1], // 60
-    [120, '1 minute ago', '1 minute from now'], // 60*2
-    [3600, 'minutes', 60], // 60*60, 60
-    [7200, '1 hour ago', '1 hour from now'], // 60*60*2
-    [86400, 'hours', 3600], // 60*60*24, 60*60
-    [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
-    [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-    [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
-    [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-    [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
-    [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-    [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
-    [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-    [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
-    [58060800000, 'centuries', 2903040000], // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
-  ]
-  let seconds = (+new Date() - time) / 1000,
-    token = 'ago',
-    list_choice = 1
+  const timeFormats = [
+    [60, 'seconds', 1],
+    [120, '1 minute ago', '1 minute from now'],
+    [3600, 'minutes', 60],
+    [7200, '1 hour ago', '1 hour from now'],
+    [86400, 'hours', 3600],
+    [172800, 'Yesterday', 'Tomorrow'],
+    [604800, 'days', 86400],
+    [1209600, 'Last week', 'Next week'],
+    [2419200, 'weeks', 604800],
+    [4838400, 'Last month', 'Next month'],
+    [29030400, 'months', 2419200],
+    [58060800, 'Last year', 'Next year'],
+    [2903040000, 'years', 29030400],
+    [5806080000, 'Last century', 'Next century'],
+    [58060800000, 'centuries', 2903040000],
+  ];
+  let seconds = (Number(new Date()) - time) / 1000;
+  let token = 'ago';
+  let listChoice = 1;
 
-  if (seconds == 0) {
-    return 'Just now'
+  if (seconds === 0) {
+    return 'Just now';
   }
   if (seconds < 0) {
-    seconds = Math.abs(seconds)
-    token = 'from now'
-    list_choice = 2
+    seconds = Math.abs(seconds);
+    token = 'from now';
+    listChoice = 2;
   }
-  let i = 0,
-    format
-  while ((format = time_formats[i++]))
+  let format = null;
+  let idx = 0;
+
+  while ((format = timeFormats[(idx += 1)])) {
     if (seconds < format[0]) {
-      if (typeof format[2] == 'string') return format[list_choice]
-      else return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token
+      if (typeof format[2] === 'string') {
+        return format[listChoice];
+      }
+
+      return `${Math.floor(seconds / format[2])} ${format[1]} ${token}`;
     }
-  return time
-}
+  }
+
+  return time;
+};
 
 export const isToday = (date, now) => {
-  console.log(date, now)
-  const yearDate = date.getYear()
-  const monthDate = date.getMonth()
-  const dayDate = date.getDate()
-  const yearNow = now.getYear()
-  const monthNow = now.getMonth()
-  const dayNow = now.getDate()
+  const yearDate = date.getYear();
+  const monthDate = date.getMonth();
+  const dayDate = date.getDate();
+  const yearNow = now.getYear();
+  const monthNow = now.getMonth();
+  const dayNow = now.getDate();
+
   if (yearDate === yearNow && monthDate === monthNow && dayDate === dayNow) {
-    return true
+    return true;
   }
-  return false
-}
+
+  return false;
+};
