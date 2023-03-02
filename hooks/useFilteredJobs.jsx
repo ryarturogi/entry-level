86eb5f77cc6e65b11cc2f10c2eb2539aba2aa1c2
Ricardo Guillen I <r.arturogi@gmail.com>
2023-03-02 10:36:23 -0400

    ♻️ TODO: remove the cacheFilters state, need to find a better way to cache the filters

--- hooks/useFilteredJobs.jsx
+++ hooks/useFilteredJobs.jsx
@@ -11,21 +11,19 @@ const useFilteredJobs = () => {
 
   const handleFiltersChange = async (filters) => {
     setLoading(true);
-    setCachedFilters((prevState) => ({ ...prevState, ...filters }));
+    setCachedFilters(filters);
 
     try {
-      let filteredJobs;
-
       // Cache filters
-      if (!isCachedFiltersEmpty(cachedFilters)) {
-        const mergedFilters = { ...cachedFilters, ...filters };
-        filteredJobs = await ClientApi.getFilteredJobs(mergedFilters);
-      } else {
-        filteredJobs = await ClientApi.getFilteredJobs(filters);
-      }
+      const mergedFilters = {
+        ...cachedFilters,
+        ...filters,
+      };
+      const filteredJobs = await ClientApi.getFilteredJobs(mergedFilters);
 
       setJobs(filteredJobs);
       setLoading(false);
+      return cachedFilters;
     } catch (error) {
       const errorMessage = error?.response?.data?.message || error.message;
       setErrors(errorMessage);
@@ -33,28 +31,10 @@ const useFilteredJobs = () => {
     }
   };
 
-  const isCachedFiltersEmpty = (obj) => {
-    const keys = Object.keys(obj);
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      const value = obj[key];
-      if (typeof value === 'object') {
-        if (Object.keys(value).length > 0) {
-          return true;
-        }
-      }
-    }
-    return false;
-  };
-
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const jobs = await ClientApi.getFilteredJobs(cachedFilters);
-
-        if (!jobs.length) {
-          throw new Error('Network response was not ok');
-        }
+        const jobs = await ClientApi.getJobs();
 
         setJobs(jobs);
         setLoading(false);
@@ -65,9 +45,9 @@ const useFilteredJobs = () => {
     };
 
     fetchJobs();
-  }, [cachedFilters]);
+  }, []);
 
-  return { jobs, loading, errors, handleFiltersChange };
+  return [jobs, loading, errors, handleFiltersChange];
 };
 
 export default useFilteredJobs;
