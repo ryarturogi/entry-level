5046affe47d9b7d608a99841d2293192d8335d3f
Ricardo Guillen I <r.arturogi@gmail.com>
2023-03-05 00:14:37 -0400

    🔥 remove redux-toolkit, everything related

--- store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import jobReducer from './jobsReducers';
-import savedJobsReducers from './savedJobsReducers';
-
-export default combineReducers({
-  jobsList: jobReducer,
-  savedJobs: savedJobsReducers,
-});
