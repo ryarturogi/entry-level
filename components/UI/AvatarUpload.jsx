be32cbc838cfeb153e998383f2ddff93a06bd3b9
Ricardo Guillen I <r.arturogi@gmail.com>
2023-03-06 21:43:20 -0400

    🪲 Fix Upload Logo uploader error state

--- components/UI/AvatarUpload.jsx
+++ components/UI/AvatarUpload.jsx
@@ -1,9 +1,9 @@
 import { PlusIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 import PropTypes from 'prop-types';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-const AvatarUpload = ({ onChange, id, name, placeholder, src }) => {
+const AvatarUpload = ({ onChange, id, name, placeholder, src, errors }) => {
   const [avatar, setAvatar] = useState(null);
   const [preview, setPreview] = useState(null);
   const [error, setError] = useState(null);
@@ -54,43 +54,62 @@ const AvatarUpload = ({ onChange, id, name, placeholder, src }) => {
     }
   };
 
+  useEffect(() => {
+    if (errors) {
+      setError(errors);
+    }
+  }, [errors]);
+
   return (
-    <div className="relative flex items-center space-x-5">
-      <label
-        className="block object-cover w-20 h-20 overflow-hidden bg-gray-300 rounded-lg cursor-pointer hover:bg-gray-300 focus:bg-gray-300"
-        htmlFor={id || 'companyLogo'}
-        onClick={handleImageClick}
-      >
-        {src || preview ? (
-          <Image
-            alt="avatar preview"
-            className="object-cover w-20 h-20"
-            height={80}
-            key={src || preview}
-            src={src || preview}
-            width={80}
-          />
-        ) : (
-          <div className="flex items-center justify-center w-full h-full text-2xl text-gray-600">
-            <PlusIcon className="w-10 h-10" />
-          </div>
-        )}
-      </label>
-      <input
-        accept="image/*"
-        className="hidden"
-        id={id || 'companyLogo'}
-        name={name || 'companyLogo'}
-        onChange={handleFileInputChange}
-        onClick={handleFileInputClick}
-        ref={fileInputRef}
-        type="file"
-      />
-      {error && <p className="mt-2 text-sm text-red-500">{error}</p>}
-      <p className="mt-2 text-base font-semibold text-gray-800 uppercase">
-        {placeholder || 'Upload your avatar'}
-      </p>
-    </div>
+    <>
+      <div className="relative flex items-center space-x-5">
+        <label
+          className={`${
+            error
+              ? 'bg-secondary-100 hover:bg-secondary-200 focus:bg-secondary-100'
+              : ' bg-gray-300 hover:bg-gray-300 focus:bg-gray-300'
+          } block object-cover w-20 h-20 overflow-hidden rounded-lg cursor-pointer`}
+          htmlFor={id || 'companyLogo'}
+          onClick={handleImageClick}
+        >
+          {src || preview ? (
+            <Image
+              alt="avatar preview"
+              className="object-cover w-20 h-20"
+              height={80}
+              key={src || preview}
+              src={src || preview}
+              width={80}
+            />
+          ) : (
+            <div
+              className={`flex items-center justify-center w-full h-full text-2xl ${
+                error ? 'text-secondary-800' : 'text-gray-600'
+              }`}
+            >
+              <PlusIcon className="w-10 h-10" />
+            </div>
+          )}
+        </label>
+        <input
+          accept="image/*"
+          className="hidden"
+          id={id || 'companyLogo'}
+          name={name || 'companyLogo'}
+          onChange={handleFileInputChange}
+          onClick={handleFileInputClick}
+          ref={fileInputRef}
+          type="file"
+        />
+        <p
+          className={`mt-2 text-base font-semibold uppercase ${
+            error ? 'text-red-600' : 'text-gray-800'
+          }`}
+        >
+          {error || placeholder || 'Upload Company Logo'}
+        </p>
+      </div>
+    </>
   );
 };
 
@@ -100,6 +119,7 @@ AvatarUpload.propTypes = {
   name: PropTypes.string,
   src: PropTypes.string,
   placeholder: PropTypes.string,
+  errors: PropTypes.object,
 };
 
 export default AvatarUpload;
