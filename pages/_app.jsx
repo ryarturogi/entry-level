49df13774b8ccbaa352a91014aa33440b5ae3176
Ricardo Guillen I <r.arturogi@gmail.com>
2023-03-06 21:21:19 -0400

    🪲 Fix setInitialData logic, wrong placement, moved to _app.jsx to handle properly the initial load.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,21 @@
 import Layout from '@/components/Layout';
+import useStore from '@/lib/store';
 import '@/styles/globals.css';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { SessionContextProvider, useUser } from '@supabase/auth-helpers-react';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
+import { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = ({ Component, pageProps }) => {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const setInitialState = useStore((state) => state.setInitialState);
+  const user = useUser();
+
+  useEffect(() => {
+    setInitialState();
+  }, [user?.id]);
 
   return (
     <SessionContextProvider
