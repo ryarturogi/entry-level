4f007c995527f4041908c18f611258aa5a9dd989
Ricardo Guillen I <r.arturogi@gmail.com>
2023-03-14 12:53:50 -0400

    ♻️ Update Hero component to handle new props

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -11,12 +11,12 @@ const Hero = ({ title = 'Guiding the next generation of devs!', action, logo }) 
   return (
     <section className="flex flex-col items-center mb-10 justify-center min-w-screen min-h-[255px] bg-hero-pattern bg-center bg-no-repeat bg-cover max-w-8xl w-full rounded-b-4xl mx-auto bg-primary-1000">
       {logo ? (
-        <div className="flex items-center justify-center w-24 h-24 mb-5 overflow-hidden bg-white rounded-full">
-          <CompanyLogo companyLogo={logo} companySlug="#" hasCompanyLogo />
+        <div className="flex items-center justify-center w-20 h-20 mb-5 overflow-hidden bg-white rounded-full">
+          <CompanyLogo companyLogo={logo} companySlug="#" hasCompanyLogo size="sm" />
         </div>
       ) : null}
       <div className="flex flex-col items-center text-white">
-        <h1 className="block text-xl font-medium text-center sm:text-2xl md:text-3xl">{title}</h1>
+        <h1 className="block text-xl font-medium text-center sm:text-xl md:text-2xl">{title}</h1>
         {action && user ? (
           <Button
             color="secondary"
@@ -24,10 +24,10 @@ const Hero = ({ title = 'Guiding the next generation of devs!', action, logo }) 
             onClick={() => action.handler()}
             rounded="md"
             size="md"
-            styles="mt-8 w-full max-w-[205px] max-h-[44px]"
+            styles="mt-8 w-full max-w-[205px] h-[40px]"
             title={action.title}
           >
-            {action.title || 'Post a Job'}
+            {action.title || 'Get Started'}
           </Button>
         ) : (
           <Button
@@ -36,7 +36,7 @@ const Hero = ({ title = 'Guiding the next generation of devs!', action, logo }) 
             onClick={() => router.push('/login')}
             rounded="md"
             size="md"
-            styles="mt-8 w-full max-w-[280px] max-h-[44px]"
+            styles="mt-8 w-full max-w-[205px] h-[40px]"
             title="Register or Login"
           >
             Join the community
