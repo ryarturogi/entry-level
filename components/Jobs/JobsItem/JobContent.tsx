0220109802f57b91bde18dce1f9fca71604e899c
Ricardo Guillen I <r.arturogi@gmail.com>
2023-03-31 00:22:36 -0400

    ♻️ Update JobsItem component to use typescript

--- components/Jobs/JobsItem/JobContent.tsx
+++ components/Jobs/JobsItem/JobContent.tsx
@@ -2,45 +2,31 @@ import React from 'react';
 import Link from 'next/link';
 import JobMeta from './JobMeta';
 import { timeSince } from '@/utils/formatDate';
-import { JobContentProps } from './types';
+import { Job } from './types';
 
-const JobContent = (props: JobContentProps): React.ReactElement => {
-  const { job } = props;
-  const {
-    id,
-    createdAt,
-    jobTitle,
-    companySlug,
-    companyName,
-    location,
-    jobTags,
-    jobCategory,
-    jobSalary,
-    jobType,
-  } = job;
-
+const JobContent = (job: Job): React.ReactElement => {
   return (
     <div className="items-center justify-between w-full lg:flex">
       <section className="flex flex-col sm:items-start sm:text-left">
-        <Link href={`/company/${companySlug}`} rel="noopener noreferrer" target="new">
-          <div className="mb-1 text-sm text-gray-500 hover:text-primary-700">{companyName}</div>
+        <Link href={`/company/${job.companySlug}`} rel="noopener noreferrer" target="new">
+          <div className="mb-1 text-sm text-gray-500 hover:text-primary-700">{job.companyName}</div>
         </Link>
-        <Link href={`/job/${id}`} rel="noopener noreferrer" target="new">
-          <div className="mb-2 text-base font-semibold hover:text-primary-700 ">{jobTitle}</div>
+        <Link href={`/job/${job.id}`} rel="noopener noreferrer" target="new">
+          <div className="mb-2 text-base font-semibold hover:text-primary-700 ">{job.jobTitle}</div>
         </Link>
 
         <JobMeta
-          jobCategory={jobCategory}
-          jobSalary={jobSalary}
-          jobTags={jobTags}
-          jobType={jobType}
-          location={location}
+          jobCategory={job.jobCategory}
+          jobSalary={job.jobSalary}
+          jobTags={job.jobTags}
+          jobType={job.jobType}
+          location={job.location}
         />
       </section>
 
       <section className="absolute top-5 right-5">
         <h1 className="flex justify-center text-sm font-light text-gray-500 lg:text-sm sm:justify-start">
-          {timeSince(createdAt)}
+          {timeSince(job.createdAt)}
         </h1>
       </section>
     </div>
